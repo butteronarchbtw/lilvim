cde968a7c42fba7717f5ac7e57bd0c30ac2c0253
butteronarchbtw <152636161+butteronarchbtw@users.noreply.github.com>
2025-06-20 00:18:39 +0200

    adapt to my own config (documentation still TODO)

--- lua/lil-editing.lua
+++ lua/lil-editing.lua
@@ -6,8 +6,8 @@
 ---
 ---@keymaps
 --- |NORMAL|
---- <leader>y : Copy to clipboard
---- <leader>p : Paste from clipboard
+--- <leader>yy : Copy to clipboard
+--- <leader>pp : Paste from clipboard
 --- j/k       : Navigate wrapped lines
 --- gcc       : Comment line
 --- |VISUAL|
@@ -17,6 +17,8 @@
 -- Indentation
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
+vim.o.softtabstop = 4
+vim.o.smarttab = true
 
 -- Continue wrapped lines with matching indentation
 vim.o.breakindent = true
@@ -29,14 +31,43 @@ vim.o.showbreak = [[\\]]
 vim.o.undofile = true
 
 -- Start scrolling before reaching the edge of the screen
-vim.o.scrolloff = 3
+vim.o.scrolloff = 10
+
+-- Highlight spaces, tabs and so on
+vim.opt.list = true
+vim.opt.listchars = "eol:↲,trail:󱁐,tab:» ,space:·"
+
+-- Highlight line the cursor is on
+vim.o.cursorline = true
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- nvim does not call InsertLeave on <C-c>
+-- which is problematic because <Esc> does
+-- e.g. diagnostics do get displayed on InsertLeave which would not happen with
+-- <C-c> normally, but with <Esc>
+vim.keymap.set("i", "<C-c>", "<Esc>")
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
 
 -- Copy and paste via clipboard
-vim.keymap.set({ "n", "v" }, "<leader>y", '"+y', { desc = "Copy to clipboard" })
-vim.keymap.set({ "n", "v" }, "<leader>p", '"+p', { desc = "Paste from clipboard" })
+vim.keymap.set({ "n", "v" }, "<leader>yy", '"+y', { desc = "Copy to clipboard" })
+vim.keymap.set({ "n", "v" }, "<leader>pp", '"+p', { desc = "Paste from clipboard" })
 
 -- Navigate through wrapped lines via j/<down>, k/<up>
 vim.keymap.set({ "n", "v" }, "j", 'v:count || mode(1)[0:1] == "no" ? "j" : "gj"', { expr = true })
 vim.keymap.set({ "n", "v" }, "k", 'v:count || mode(1)[0:1] == "no" ? "k" : "gk"', { expr = true })
 vim.keymap.set({ "n", "v" }, "<up>", 'v:count || mode(1)[0:1] == "no" ? "k" : "gk"', { expr = true })
 vim.keymap.set({ "n", "v" }, "<down>", 'v:count || mode(1)[0:1] == "no" ? "j" : "gj"', { expr = true })
+
+-- Indent line with selection
+vim.keymap.set('v', '<', '<gv', { desc = 'Dedent line' })
+vim.keymap.set('v', '>', '>gv', { desc = 'Indent line' })
+
+-- Ident whole file at once
+vim.keymap.set('n', '=', 'mxgg=G`x', { desc = 'Indent file' })
--- lua/lil-search.lua
+++ lua/lil-search.lua
@@ -30,13 +30,13 @@
 vim.o.ignorecase = true
 vim.o.smartcase = true
 
-vim.keymap.set("n", "<leader>e", ":Ex<cr>", { desc = "Open file explorer" })
+vim.keymap.set("n", "<leader>pv", ":Ex<cr>", { desc = "Project View" })
 
 if vim.fn.executable("fzf") == 1 then
 	local fzf_bin_path = vim.fn.exepath("fzf")
 	if fzf_bin_path ~= "" then
 		local fzf_plugin_path = string.gsub(fzf_bin_path, "bin", "opt")
 		vim.opt.runtimepath:append(fzf_plugin_path)
-		vim.keymap.set("n", "<leader>f", ":FZF<cr>", { desc = "Fuzzy find" })
+		vim.keymap.set("n", "<leader>pf", ":FZF<cr>", { desc = "Project Find" })
 	end
 end
